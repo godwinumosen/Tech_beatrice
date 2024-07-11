f0d8303bea4da7c6138a974e54c73c3b5079a0bc
Godwin Umosen <umosengodwin568@gmail.com>
2024-07-11 01:05:26 -0700

    Static file is been implemented into the techbeatrice app

--- techbeatrice/views.py
+++ techbeatrice/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home (request):
-    return render (request, 'home.html' )
+    return render (request, 'techbeatrice/home.html' )
 
 def base (request):
     return render(request,"base.html")
