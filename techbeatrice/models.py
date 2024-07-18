59ca6240ea5542512d007909a65945e141cf06e7
Godwin Umosen <umosengodwin568@gmail.com>
2024-07-18 02:14:06 -0700

    Accessing the dues website

--- techbeatrice/models.py
+++ techbeatrice/models.py
@@ -6,8 +6,11 @@ from datetime import datetime, date
 
 class Techbeatrice_Subjects(models.Model):      
     techbeatrice_subjects_title = models.CharField(max_length =205, blank=True, null=True)
+    techbeatrice_subjects_sub_title = models.CharField(max_length =205, blank=True, null=True)
     techbeatrice_subjects_description = models.TextField()
+    techbeatrice_subjects_sub_description = models.TextField()
     techbeatrice_subjects_img = models.ImageField(upload_to ='tech_subjects_img/',blank=True,null=True)
+    techbeatrice_subjects_sub_img = models.ImageField(upload_to ='tech_subjects_sub_img/',blank=True,null=True)
     techbeatrice_subjects_slug = models.SlugField (max_length =205,blank=True, null=True)
     techbeatrice_subjects_publish_date = models.DateTimeField (auto_now_add= True)
     techbeatrice_subjects_author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -22,9 +25,12 @@ class Techbeatrice_Subjects(models.Model):
 # The main model for The techbeatrice Model category
 class Techbeatrice_Courses_PostModel(models.Model):
     techbeatrice_courses_title = models.CharField(max_length =255, blank=True, null=True)
+    techbeatrice_courses_sub_title = models.CharField(max_length =255, blank=True, null=True)
     techbeatrice_courses_cost_price = models.IntegerField(default ='0', blank=True, null=True)
     techbeatrice_courses_description = models.TextField()
+    techbeatrice_courses_sub_description = models.TextField()
     techbeatrice_courses_img = models.ImageField(upload_to ='tech_courses_img/',blank=True,null=True)
+    techbeatrice_courses_sub_img = models.ImageField(upload_to ='tech_courses_sub_img/',blank=True,null=True)
     techbeatrice_courses_slug = models.SlugField (max_length =255,blank=True, null=True)
     techbeatrice_courses_publish_date = models.DateTimeField (auto_now_add= True)
     techbeatrice_courses_author = models.ForeignKey(User, on_delete=models.CASCADE)
