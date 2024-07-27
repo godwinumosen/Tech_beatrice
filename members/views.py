47711c5723bcce6935f8b2c9d9ffbea0bab140f7
Godwin Umosen <umosengodwin568@gmail.com>
2024-07-27 09:39:22 -0700

    Article details of the event page is been implement to the event details

--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def login_user(request):
     if request.method == "POST":
@@ -13,9 +14,12 @@ def login_user(request):
             return redirect('home')
         else:
             messages.success(request, ("There Was An Error Loggin In, Try Again..."))
-            return redirect('login')
-
-    
+            return redirect('login')    
     else:
         return render(request, 'authenticate/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You"))
+    return redirect('home')
 
