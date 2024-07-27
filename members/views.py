f76ff9450c0979ac4eab7f02479c425e7c956c15
Godwin Umosen <umosengodwin568@gmail.com>
2024-07-27 09:26:38 -0700

    Article details of the event page is been implement to the event details

--- members/views.py
+++ members/views.py
@@ -3,5 +3,19 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('home')
+        else:
+            messages.success(request, ("There Was An Error Loggin In, Try Again..."))
+            return redirect('login')
+
+    
+    else:
+        return render(request, 'authenticate/login.html', {})
 
